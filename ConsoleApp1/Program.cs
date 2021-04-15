6f615fa9a8686810b662307b23f35a318f1df4cd
Varuna Lakshan <varunalakshan7788@gmail.com>
2021-04-15 19:11:38 +0530

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static IHelloRemotingService.IHelloRemtingService client;
+        static void Main(string[] args)
+        {
+            TcpChannel channel = new TcpChannel();
+            ChannelServices.RegisterChannel(channel);
+            client = (IHelloRemotingService.IHelloRemtingService)Activator.GetObject(
+              typeof(IHelloRemotingService.IHelloRemtingService),
+              "tcp://localhost:8080/GetMessage");
+           Console.WriteLine(client.GetMessage("varuna"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RemotingServiceHost/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemotingServiceHost
+{
+    class Program
+    {
+        static void Main()
+        {
+            HelloRemotingService.HelloRemotingService remotingService =
+                new HelloRemotingService.HelloRemotingService();
+            TcpChannel channel = new TcpChannel(8080);
+            ChannelServices.RegisterChannel(channel);
+            RemotingConfiguration.RegisterWellKnownServiceType(
+               typeof(HelloRemotingService.HelloRemotingService), "GetMessage",
+               WellKnownObjectMode.Singleton);
+            Console.WriteLine("Host started @ " + DateTime.Now.ToString());
+            Console.ReadLine();
+        }
+    }
+}
